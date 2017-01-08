c7cc02cd7c6718e44cd9c1bef7d913b8bc764d44
Matthew Tanudjaja <mrexmelle@gmail.com>
2017-01-08 19:44:57 +0700

    feat: Enable file upload

--- src/main/java/com/linecorp/example/springcloudinary/ImageController.java
+++ src/main/java/com/linecorp/example/springcloudinary/ImageController.java
@@ -5,18 +5,26 @@ import com.cloudinary.Cloudinary;
 import com.cloudinary.api.ApiResponse;
 import com.cloudinary.utils.ObjectUtils;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import org.cloudinary.json.JSONArray;
+import org.cloudinary.json.JSONObject;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@RequestMapping(value="/")
+@RequestMapping(value="/cloud")
 public class ImageController
 {
     @Autowired
@@ -40,18 +48,41 @@ public class ImageController
         try
         {
             Map response=c.api().resource("", ObjectUtils.asMap("type", "upload"));
-            List<Object> resList = (List<Object>)(response.get("resources"));
-
-            for(Object o : resList)
+            JSONObject json=new JSONObject(response);
+            JSONArray ja=json.getJSONArray("resources");
+            for(int i=0; i<ja.length(); i++)
             {
-                Map m=(Map)(o);
-                retval.add((String)(m.get("url")));
+                JSONObject j=ja.getJSONObject(i);
+                retval.add(j.getString("url"));
             }
+
+            return new ResponseEntity< List<String> >(retval, HttpStatus.OK);
         }
         catch (Exception e)
         {
-
+            return new ResponseEntity< List<String> >(HttpStatus.BAD_REQUEST);
         }
-        return new ResponseEntity< List<String> >(retval, HttpStatus.OK);
+    }
+
+    @PostMapping(value="/image")
+    public ResponseEntity< String > post(
+        @RequestParam(value="upload", required=true) MultipartFile aFile)
+    {
+        Cloudinary c=new Cloudinary("cloudinary://"+mApiKey+":"+mApiSecret+"@"+mCloudName);
+
+        try
+        {
+            File f=Files.createTempFile("temp", aFile.getOriginalFilename()).toFile();
+            aFile.transferTo(f);
+
+            Map response=c.uploader().upload(f, ObjectUtils.emptyMap());
+            JSONObject json=new JSONObject(response);
+            String url=json.getString("url");
+            return new ResponseEntity<String>("{\"status\":\"OK\", \"url\":\""+url+"\"}", HttpStatus.OK);
+        }
+        catch(Exception e)
+        {
+            return new ResponseEntity< String >("", HttpStatus.BAD_REQUEST);
+        }
     }
 };
